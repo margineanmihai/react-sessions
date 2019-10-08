22703a4438a90fa93a3a8426b133ef54eced48cf
anca.raduta <anca.raduta@3pillarglobal.com>
2019-10-08 15:14:30 +0300

    class components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from "./components/Person";
 
 
-const App = () => {
-  return (
+class App extends Component {
+
+  state = {
+    persons: [
+      {name: "Ana", age: 20},
+      {name: "Maria", age: 25, info: "My hobbies are reading and hiking."}
+    ],
+    title: "My React App"
+  };
+
+  addPersonHandler = () => {
+    this.setState({
+      persons: [
+        ...this.state.persons,
+        {name: "Grigore", age: 75, info: "Reaching the end... :'("}
+      ]
+    });
+  };
+
+  render() {
+    const { persons, title } = this.state;
+
+    return (
       <div className="App">
-          <Person name="Ana" age="20"/>
-          <Person name="Maria" age="25">
-              <p>My hobbies are reading and hiking.</p>
-          </Person>
+        <h1>{title}</h1>
+        <button onClick={this.addPersonHandler}>Add Person</button>
+        {
+          persons.map(person =>
+              <Person key={person.name} name={person.name} age={person.age}>
+                <p>{person.info}</p>
+              </Person>
+            )
+        }
       </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
