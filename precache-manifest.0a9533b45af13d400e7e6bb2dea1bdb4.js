1c37b3a5f8b106e7c10947f7ef97ced284e049b3
Mihai Marginean <marginean.mihai@gmail.com>
2019-11-06 18:37:23 +0200

    Updates

---
+++ precache-manifest.0a9533b45af13d400e7e6bb2dea1bdb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7bfc24c5df371f2fbcb77854ead6732",
+    "url": "/react-sessions/index.html"
+  },
+  {
+    "revision": "c5575d1f6509c0b7b231",
+    "url": "/react-sessions/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "32dcc9285ed7ec6172ae",
+    "url": "/react-sessions/static/js/2.2f892734.chunk.js"
+  },
+  {
+    "revision": "c5575d1f6509c0b7b231",
+    "url": "/react-sessions/static/js/main.ef9e454d.chunk.js"
+  },
+  {
+    "revision": "06b542a9183e5bc3392e",
+    "url": "/react-sessions/static/js/runtime-main.85c31bc5.js"
+  }
+]);
